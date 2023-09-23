2e452c0d7b401f96df721911ad5a05eebecfd34b
Marcelo Castelo <marcelo.cbo2000@gmail.com>
2023-09-23 16:23:17 -0300

    centralizando

--- App.js
+++ App.js
@@ -5,28 +5,17 @@ const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
 
 const App = () => (
   <View style={styles.container}>
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <Text style={styles.text}>Inside</Text>
-    </ImageBackground>
+    <Text>Hello World!</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  image: {
-    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
-  },
+
 });
 
 export default App;
