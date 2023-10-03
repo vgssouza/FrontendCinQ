99f64e31c621301b76fa94fa44599bf9c0753068
Marcelo Castelo <marcelo.cbo2000@gmail.com>
2023-10-03 15:56:00 -0300

    texto formatado

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ import {
   Pressable,
   Text,
 } from "react-native";
-import {Link} from 'expo-router'
+import { Link } from "expo-router";
 
 const App = () => (
   <View style={styles.container}>
@@ -16,27 +16,28 @@ const App = () => (
       source={require("./../assets/images/backgroundPreto.png")}
       resizeMode="cover"
     >
-      
-      <Image style={styles.logoSize} source={require("./../assets/images/logoBranca.png")} />
-    
-    <View style={styles.botao}>
-      <View style={styles.botaoDentro}>
-   <Link href="/login/" asChild>
-      <Pressable>
-        <Text style={{color:"white"}}>LOGIN</Text>
-      </Pressable>
-    </Link>
-    </View>
+      <Image
+        style={styles.logoSize}
+        source={require("./../assets/images/logoBranca.png")}
+      />
 
-    <View style={styles.botaoDentro}>
-    <Link href="/registrar/" asChild>
-      <Pressable>
-      <Text style={{padding: 20, color:"white"}}>REGISTRAR</Text>
-      </Pressable>
-    </Link>
-    </View>
-    </View>
-      
+      <View style={styles.botao}>
+        <View style={styles.botaoDentro}>
+          <Link href="/login/" asChild>
+            <Pressable>
+              <Text style={{ color: "white" }}>LOGIN</Text>
+            </Pressable>
+          </Link>
+        </View>
+
+        <View style={styles.botaoDentro}>
+          <Link href="/registrar/" asChild>
+            <Pressable>
+              <Text style={{ padding: 20, color: "white" }}>REGISTRAR</Text>
+            </Pressable>
+          </Link>
+        </View>
+      </View>
     </ImageBackground>
   </View>
 );
@@ -47,7 +48,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
-
   botao: {
     flex: 1,
     alignSelf: "center",
@@ -56,15 +56,15 @@ const styles = StyleSheet.create({
 
   botaoDentro: {
     marginTop: "30%",
-  width: "92.5%",
-  flex: 1,
-  padding: "1.2%",
-  alignSelf: "center",
-  backgroundColor: "#151D2D",
-  alignContent: "center",
-  alignItems: "center",
-  justifyContent: "center",
-  borderRadius: 5,
+    width: "92.5%",
+    flex: 1,
+    padding: "1.2%",
+    alignSelf: "center",
+    backgroundColor: "#151D2D",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
   },
 
   logoSize: {
--- app/registrar/index.tsx
+++ app/registrar/index.tsx
@@ -8,63 +8,61 @@ import {
   Text,
   Pressable,
 } from "react-native";
-import {Link} from 'expo-router'
+import { Link } from "expo-router";
 import { TextInput } from "react-native-gesture-handler";
 
 const login = () => (
   <View>
-   
-   <Image 
-   style={styles.logoSize}
-   source={require("./../../assets/images/logoColorida.png")} />
-   
-   <Text
-   style={styles.title}
-   >Cadastre-se</Text>
-  
-  <View style={styles.divInput}>
-  <Text style={{marginTop: "2%", fontSize: 15, fontWeight: "bold"}}>Nome*</Text>
-   <TextInput
-        style={styles.input}
-        placeholder="Insira seu nome"
-      />
-  </View>
+    <Image
+      style={styles.logoSize}
+      source={require("./../../assets/images/logoColorida.png")}
+    />
 
-  <View style={styles.divInput}>
-  <Text style={{fontSize: 15, fontWeight: "bold"}}>Email*</Text>
-       <TextInput
-        style={styles.input}
-        placeholder="Insira seu email"
-      />
-  </View>
+    <Text style={styles.title}>Cadastre-se</Text>
 
-  <View style={styles.botaoDentro}>
-   <Link href="/home/" asChild>
-      <Pressable>
-        <Text style={{color:"white"}}>ENTRAR</Text>
-      </Pressable>
-    </Link>
-    </View>
-   
-    <View style={{
-  marginTop: "5%",
-  flex: 1,
-  alignSelf: "center",
-  alignItems: "center",
-
-  }}>
-   <Link href="/home/" asChild>
-      <Pressable>
-        <Image source={require("./../../assets/images/Google.png")} />
-      </Pressable>
-    </Link>
+    <View style={styles.divInput}>
+      <Text style={{ marginTop: "2%", fontSize: 15, fontWeight: "bold" }}>
+        Nome*
+      </Text>
+      <TextInput style={styles.input} placeholder="Insira seu nome" />
     </View>
 
-    <Text style={{marginTop:"5%", textAlign:"center", fontSize: 15}}>Já possui uma conta? <Link style={{fontWeight:"bold", color:"#151D2D"}} href="/login/">Faça o login</Link> </Text>
-    
+    <View style={styles.divInput}>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>Email*</Text>
+      <TextInput style={styles.input} placeholder="Insira seu email" />
+    </View>
+
+    <View style={styles.botaoDentro}>
+      <Link href="/home/" asChild>
+        <Pressable>
+          <Text style={{ color: "white" }}>ENTRAR</Text>
+        </Pressable>
+      </Link>
+    </View>
+
+    <View
+      style={{
+        marginTop: "5%",
+        flex: 1,
+        alignSelf: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href="/home/" asChild>
+        <Pressable>
+          <Image source={require("./../../assets/images/Google.png")} />
+        </Pressable>
+      </Link>
+    </View>
+
+    <Text style={{ marginTop: "5%", textAlign: "center", fontSize: 15 }}>
+      Já possui uma conta?{" "}
+      <Link style={{ fontWeight: "bold", color: "#151D2D" }} href="/login/">
+        Faça o login
+      </Link>{" "}
+    </Text>
   </View>
 );
-
 
 const styles = StyleSheet.create({
   input: {
@@ -83,30 +81,30 @@ const styles = StyleSheet.create({
     marginTop: "20%",
     opacity: 1,
   },
-  
+
   title: {
-  flex: 1,
-  alignSelf: "center",
-  fontSize: 30,
-  fontWeight: "bold",
-  marginTop: "20%",
+    flex: 1,
+    alignSelf: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: "20%",
   },
 
   divInput: {
-  padding: "2%",
+    padding: "2%",
   },
 
   botaoDentro: {
     marginTop: "5%",
-  width: "92.5%",
-  flex: 1,
-  padding: "1.2%",
-  alignSelf: "center",
-  backgroundColor: "#151D2D",
-  alignContent: "center",
-  alignItems: "center",
-  justifyContent: "center",
-  borderRadius: 5,
-  }
+    width: "92.5%",
+    flex: 1,
+    padding: "1.2%",
+    alignSelf: "center",
+    backgroundColor: "#151D2D",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+  },
 });
 export default login;
